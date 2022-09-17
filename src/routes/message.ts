3deaa20f52d7a01c35ceed385146983ad4bac312
Udoka <udochukwukaonyela@gmail.com>
2022-09-17 17:09:43 +0100

    fixes

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -3,25 +3,45 @@ require('dotenv').config();
 import { Request, Response, Router } from "express";
 const router = Router();
 router.post('/send', async (req: Request, res: Response, next) => {
-    const { body, email } = req.body;
-    const courier = CourierClient(
-        { authorizationToken: process.env.COURIER_AUTH_TOKEN });
+    const { message, email, first_name, last_name, title } = req.body;
+    if (!!message && !!email && !!first_name && !!last_name && !!title) {
+        const courier = CourierClient(
+            { authorizationToken: process.env.COURIER_AUTH_TOKEN });
 
-    const data = await courier.send({
-        message: {
-            to: {
-                email: "udochukwukaonyela@gmail.com"
-            },
-            content: {
-                title: "Welcome to Courier!",
-                body: "Want to hear a joke? {{joke}}"
-            },
-            data: {
-                joke: "Why was the JavaScript developer sad? Because they didn't Node how to Express themselves"
-            },
+        const data = await courier.send({
+            message: {
+
+                to: {
+                    email: "udochukwukaonyela@gmail.com"
+                },
+                content: {
+                    title: title,
+                    body: `Hi, I'm ${first_name} ${last_name},\n ${message} \n Here's my email: ${email} \n you can contact me on this email.`,
+
+                }
+            }
+        });
+        return res.status(200).json({ message: "Message sent successfully", data });
+    }
+    else {
+        if(!email){
+            return res.status(400).json({ message: "Email is required" });
         }
-    });
-    console.log(data);
-    res.status(200).json({ message: "Message sent successfully", data });
+        if (!first_name) {
+            return res.status(400).json({ message: "First name is required" });
+        }
+        if (!last_name) {
+            return res.status(400).json({ message: "Last name is required" });
+        }
+        if (!title) {
+            return res.status(400).json({ message: "Title is required" });
+        }
+        if (!message) {
+            return res.status(400).json({ message: "Message is required" });
+        }
+        else {
+            return res.status(400).json({ message: "Something went wrong" });
+        }
+    }
 })
 module.exports = router;
