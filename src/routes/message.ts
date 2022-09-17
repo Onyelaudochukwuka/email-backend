084ef8404c2df15dbb86d9a87370882f6a5bb9be
Udoka <udochukwukaonyela@gmail.com>
2022-09-18 00:06:53 +0100

    fixes

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -5,11 +5,17 @@ import { body } from 'express-validator';
 const router = Router();
 const { SendMail } = require("../controllers/message");
 router.post('/send',
-    body('message').isString().notEmpty(),
-    body('user_email').isEmail().notEmpty(),
-    body('first_name').isString().notEmpty(),
-    body('last_name').isString().notEmpty(),
-    body('title').isString().notEmpty(),
-    body('my_email').isEmail().notEmpty(),
+    body('message', "A message from the user is required").isString(),
+    body('message', "Field Cannot be empty").notEmpty(),
+    body('user_email', "An email from the user is required").isEmail(),
+    body('user_email', "Field Cannot be empty").notEmpty(),
+    body('first_name', "The first Name of the user is required").isString(),
+    body('first_name', "Field Cannot be empty").notEmpty(),
+    body('last_name', "The last name is required").isString(),
+    body('last_name', "Field Cannot be empty").notEmpty(),
+    body('title', "Your message must have a title").isString(),
+    body('title', "Field Cannot be empty").notEmpty(),
+    body('my_email', "An email from user is required").isEmail(),
+    body('my_email', "Field Cannot be empty").notEmpty(),
     SendMail)
 module.exports = router;
