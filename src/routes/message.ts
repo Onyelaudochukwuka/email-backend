431abb6818408919137fc2759d4dc0dfe8497529
Udoka <udochukwukaonyela@gmail.com>
2022-09-17 23:31:03 +0100

    fixes

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -4,6 +4,7 @@ import { Request, Response, Router } from "express";
 const router = Router();
 interface Res {
     message: string;
+    success: boolean;
     data?: {
         requestId: string;
     }
@@ -35,27 +36,10 @@ router.post('/send', async (req: Request<Req>, res: Response<Res>, next) => {
                 }
             }
         });
-        return res.status(200).json({ message: "Message sent successfully", data });
+        return res.status(200).json({ success: true, message: "Message sent successfully", data });
     }
     else {
-        if (!user_email){
-            return res.status(400).json({ message: "Email is required" });
-        }
-        if (!first_name) {
-            return res.status(400).json({ message: "First name is required" });
-        }
-        if (!last_name) {
-            return res.status(400).json({ message: "Last name is required" });
-        }
-        if (!title) {
-            return res.status(400).json({ message: "Title is required" });
-        }
-        if (!message) {
-            return res.status(400).json({ message: "Message is required" });
-        }
-        else {
-            return res.status(400).json({ message: "Something went wrong" });
-        }
+        return res.status(400).json({ success: false, message: "Please fill all fields" });
     }
 })
 module.exports = router;
