429e5043418a8dc2420b425e9f255850695624bf
Udoka <udochukwukaonyela@gmail.com>
2022-10-03 15:25:56 +0100

    added new route

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -19,6 +19,12 @@ interface Req {
     title: string;
     my_email: string;
 }
+interface MessageOnlyReq {
+    message: string;
+    user_email: string;
+    user_name: string;
+    my_email: string;
+}
 
 exports.SendMail = async (req: Request<Req>, res: Response<Res>) => {
     const errors = validationResult(req);
@@ -38,6 +44,30 @@ exports.SendMail = async (req: Request<Req>, res: Response<Res>) => {
             content: {
                 title: title,
                 body: `Hi, I'm ${first_name} ${last_name},\n ${message} \n Here's my email: ${user_email} \n you can contact me on this email.`,
+
+            }
+        }
+    });
+    return res.status(200).json({ success: true, message: "Message sent successfully", data });
+}
+exports.sendMessageOnly = async (req: Request<MessageOnlyReq>, res: Response<Res>) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array(), success: false, message: "Invalid data" });
+    }
+    const { message, user_email, user_name, my_email }: MessageOnlyReq = req.body;
+    const courier = CourierClient(
+        { authorizationToken: process.env.COURIER_AUTH_TOKEN });
+
+    const data = await courier.send({
+        message: {
+
+            to: {
+                email: my_email
+            },
+            content: {
+                title: "Message from portfolio",
+                body: `Hi, I'm ${user_name},\n ${message} \n Here's my email: ${user_email} \n you can contact me on this email.`,
 
             }
         }
