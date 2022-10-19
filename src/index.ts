d65d93ad97bfaec9928cf2d2acf9af58ae320422
Udoka <udochukwukaonyela@gmail.com>
2022-10-19 19:32:41 +0100

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application } from "express";
 require("dotenv").config();
-const cors = require('cors');
+const cors = require("cors");
 interface Server {
     init?: () => void;
 }
@@ -12,9 +12,9 @@ type Options = {
 const Options = { limit: "10mb", extended: true };
 
 const app: Application = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 // Define routes
-app.use('/message', require("./routes/message"));
+app.use("/message", require("./routes/message"));
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => console.log(`port started on Port ${PORT}`))
+app.listen(PORT, () => console.log(`port started on Port ${PORT}`));
